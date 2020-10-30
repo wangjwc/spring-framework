9ca1444ad50634cf13d8570937660f31d0016716
wangjingwang <77qweasd1997>
2020-10-30 21:01:50 +0800

    AspectJTest

---
+++ spring-learn/src/main/java/org/springframework/learn/aop/AspectJTest.java
@@ -0,0 +1,11 @@
+package org.springframework.learn.aop;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author wangjingwang
+ * @version v1.0
+ */
+@org.aspectj.lang.annotation.Aspect
+public class AspectJTest {
+}
